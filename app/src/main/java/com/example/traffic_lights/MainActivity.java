48f0802fa8b68742b97b70395792909f6c945660
hunghly <hunghly@gmail.com>
2020-10-08 21:58:20 -0500

    added version two of the traffic light app

--- app/src/main/java/com/example/traffic_lights/MainActivity.java
+++ app/src/main/java/com/example/traffic_lights/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.traffic_lights;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +10,14 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private ImageView redLight;
+    private ImageView yellowLight;
+    private ImageView greenLight;
+    private Button redBtn;
+    private Button yellowBtn;
+    private Button greenBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,20 +25,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         /* Get your ImageView elements */
-        final ImageView redLight = findViewById(R.id.red_light);
+       /* final ImageView redLight = findViewById(R.id.red_light);
         final ImageView yellowLight = findViewById(R.id.yellow_light);
         final ImageView greenLight = findViewById(R.id.green_light);
         Button redBtn = findViewById(R.id.red_btn);
         Button yellowBtn = findViewById(R.id.yellow_btn);
-        Button greenBtn = findViewById(R.id.green_btn);
+        Button greenBtn = findViewById(R.id.green_btn);*/
 
-        final int redLightImg = getResources().getIdentifier("@drawable/red_on", null, getPackageName());
+
+/*        final int redLightImg = getResources().getIdentifier("@drawable/red_on", null, getPackageName());
         final int yellowLightImg = getResources().getIdentifier("@drawable/yellow_on", null, getPackageName());
         final int greenLightImg = getResources().getIdentifier("@drawable/green_on", null, getPackageName());
-        final int offLightImg = getResources().getIdentifier("@drawable/light_off", null, getPackageName());
+        final int offLightImg = getResources().getIdentifier("@drawable/light_off", null, getPackageName());*/
 
 
-        redBtn.setOnClickListener(
+        /*redBtn.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -61,6 +70,52 @@ public class MainActivity extends AppCompatActivity {
                         greenLight.setImageResource(greenLightImg);
                     }
                 }
-        );
+        );*/
+
+
+        // Version 2
+        redLight = findViewById(R.id.red_light);
+        yellowLight = findViewById(R.id.yellow_light);
+        greenLight = findViewById(R.id.green_light);
+        redBtn = findViewById(R.id.red_btn);
+        yellowBtn = findViewById(R.id.yellow_btn);
+        greenBtn = findViewById(R.id.green_btn);
+
+        // Set on click listener by implementing on onClickListener
+        redBtn.setOnClickListener(this);
+        yellowBtn.setOnClickListener(this);
+        greenBtn.setOnClickListener(this);
+        turnOffLights();
+    }
+
+    @Override
+    public void onClick(View v) {
+        // check what was clicked
+        turnOffLights();
+        if (v == redBtn) {
+            turnOnRedLight();
+        } else if (v == yellowBtn) {
+            turnOnYellowLight();
+        } else if (v == greenBtn) {
+            turnOnGreenLight();
+        }
+    }
+
+    private void turnOffLights() {
+        redLight.setImageResource(R.drawable.light_off);
+        greenLight.setImageResource(R.drawable.light_off);
+        yellowLight.setImageResource(R.drawable.light_off);
+    }
+
+    private void turnOnRedLight() {
+        redLight.setImageResource(R.drawable.red_on);
+    }
+
+    private void turnOnYellowLight() {
+        yellowLight.setImageResource(R.drawable.yellow_on);
+    }
+
+    private void turnOnGreenLight() {
+        greenLight.setImageResource(R.drawable.green_on);
     }
 }
